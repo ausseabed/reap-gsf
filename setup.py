d3b2d17b6b804055627ca9dae86f96c706dac745
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-10-19 13:55:16 +1100

    Initial commit of pyproject.toml setup.cfg setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reap_gsf",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    url="https://github.com/ausseabed/reap-gsf",
+    author="AusSeabed",
+    description="Prototype module for unpacking the contents of a GSF file",
+    keywords=[
+        "bathymetry",
+        "GSF",
+    ],
+    # packages=find_packages(),
+    packages=["reap_gsf"],
+    install_requires=[
+        "numpy",
+        "attrs",
+        "pandas",
+        "structlog",
+    ],
+    license="Apache",
+    zip_safe=False,
+)
